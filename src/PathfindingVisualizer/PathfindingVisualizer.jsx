d997ba6026bf38bc29e9b5f8bc35b87bff94abc9
daniel <g.thirteen333@gmail.com>
2019-10-24 21:50:22 -0400

    grid shell complete

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -4,13 +4,33 @@ import Node from './Node/Node';
 export default class PathfindingVisuablizer extends Component {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {nodes: []};
     }
+
+    componentDidMount(){
+        const nodes = [];
+        for (let row = 0; row < 50; row++) {
+           const currentRow = [];
+           for (let col = 0; col < 50; col++) {
+               currentRow.push([])
+           }
+            nodes.push(currentRow)
+        }
+        this.setState({nodes})
+    }
+
     render(){
+        const {nodes} = this.state;
+        console.log(nodes)
         return (
-            <div>
-                Path Finder
-                <Node></Node>
+            <div className="grid">
+                <h1>Path Finder</h1>
+                {nodes.map((row, idx) => {
+                    return <div className="nodes">
+                        {row.map((node, nodeIdx) => <Node></Node>)}
+                    </div>
+                })}
+                
             </div>
         )
     }
