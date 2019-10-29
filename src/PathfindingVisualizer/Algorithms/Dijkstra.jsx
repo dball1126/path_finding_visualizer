bb04651a8deab034feec81798663717e3a171e0a
daniel <g.thirteen333@gmail.com>
2019-10-28 20:04:45 -0400

    dijkstra shell complete

--- src/PathfindingVisualizer/Algorithms/Dijkstra.jsx
+++ src/PathfindingVisualizer/Algorithms/Dijkstra.jsx
@@ -4,7 +4,19 @@ export const dijkstra = (nodes, start, end) => {
     if (!start || !end || start === end) return false;
     start.distance = 0;
 
-   
+   while (unvisited.length > 0){
+    sortNodesDistance(unvisited);
+        let closest = unvisited.shift();
+        
+        if (closest.distance === Infinity) return visited;
+        
+        closest.visited = true;
+        visited.push(closest);
+
+        if (closest.distance === end.distance) return visited;
+
+        updateVisitedNeighbors(closest, nodes);
+    }
 }   
 
 function getAllNodes(nodes) {
@@ -16,4 +28,28 @@ function getAllNodes(nodes) {
         }
     }
     return nodes;
+}
+
+function sortNodesDistance(unvisited){
+    return unvisited.sort((node1, node2) => node1.distance - node2.distance);
+}
+
+function getUnvisitedNeighBors(node, nodes){
+    const neighbors = [];
+    const {row, col} = node;
+
+    if (row > 0) neighbors.push(nodes[row - 1][col]);
+    if (row < nodes.length - 1) neighbors.push(nodes[row + 1][col]);
+    if (col > 0) neighbors.push(nodes[row][col - 1]);
+    if (col < nodes[0].length - 1) neighbors.push(nodes[row][col + 1]);
+    return neighbors.filter(neighbor => !neighbor.visited);
+
+}
+
+function updateVisitedNeighbors(node, nodes){
+    const unvisited = getUnvisitedNeighBors(node, nodes);
+    for (ele in nodes) {
+        ele.distance = node.distance + 1;
+        ele.previous = node;
+    }
 }
